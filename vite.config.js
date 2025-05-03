8dcbc2103d3edc0eeee0da98ba8346ad54980588
v z <vahehz@gmail.com>
2025-05-02 21:13:40 -0700

    change base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/e-plantShopping/', // Required for GitHub Pages
+  base: '/', // Changed for DigitalOcean deployment
   define: {
     'process.env': {},
     global: 'window',
