9c10f1ce6b0de1c41fa84435870e6d3ed0df4583
v z <vahehz@gmail.com>
2025-05-02 21:06:53 -0700

    deploy

---
+++ src/polyfills.js
@@ -0,0 +1,5 @@
+import { Buffer } from 'buffer';
+import 'crypto-browserify';
+
+window.Buffer = Buffer;
+window.global = window;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: '/e-plantShopping/', // Required for GitHub Pages
+  define: {
+    'process.env': {},
+    global: 'window',
+  },
+  build: {
+    rollupOptions: {
+      external: ['crypto'],
+    },
+  },
   plugins: [react()],
-})
+});
